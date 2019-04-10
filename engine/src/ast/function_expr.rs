1c6b1507cb8b5d7021852cabb56a7b155e2033ac
Ingvar Stepanyan <me@rreverser.com>
2019-04-10 15:12:26 +0100

    Simplify `echo_function` in tests
    Implementation can just return the first input argument.

--- engine/src/ast/function_expr.rs
+++ engine/src/ast/function_expr.rs
@@ -202,8 +202,8 @@ fn test_function() {
     use scheme::UnknownFieldError;
     use types::Type;
 
-    fn echo_function<'a>(_: FunctionArgs<'_, 'a>) -> LhsValue<'a> {
-        false.into()
+    fn echo_function<'a>(args: FunctionArgs<'_, 'a>) -> LhsValue<'a> {
+        args.next().unwrap()
     }
 
     lazy_static! {
